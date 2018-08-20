b94fc1947a790be2e492c8da89ff8aaf4e93c180
Justin Johnson <justin.johnson@smartbear.com>
2018-08-19 22:29:40 -0400

    fixing lint error

--- lambda/UserService/CreateUser/models/user.model.js
+++ lambda/UserService/CreateUser/models/user.model.js
@@ -2,10 +2,10 @@ const Validator = require('jsonschema').Validator;
 const UserValidator = new Validator();
 
 // pattern matching
-const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
-const urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/
+const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+const urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
 const longLatRegex = /^-?\d{1,3}(.\d*)?$/m;
-const timeOffsetRegex = /^-?\d{1,2}:\d{2}$/m
+const timeOffsetRegex = /^-?\d{1,2}:\d{2}$/m;
 // the random user api has 8 digit phone numbers
 // we are using a very simple validation of 1 or more digits, requirement unknown
 const phoneRegex = /^\d+$/m;
@@ -210,7 +210,7 @@ const userSchema = {
 
 
 const validationErrors = (user) => {
-  const { errors } = UserValidator.validate(user, userSchema)
+  const { errors } = UserValidator.validate(user, userSchema);
   return errors.map(err => err.stack);
 }
 
